fd9760c40dd1c1fecc47a599790d42c8192add5b
FranciscoDA <franciscoda@outlook.com>
2018-06-28 20:05:28 -0300

    algorithm: use learning rate

--- gs_classifier.py
+++ gs_classifier.py
@@ -3,56 +3,67 @@
 import numpy as np
 from gausseidel import gauss_seidel
 import math
-import time
 
-def gs_classifier_train(A, b, maxloss=0.5, maxiters=None):
-	'Train a linear classifier from a matrix of training data (A) and training classes (b)'
+def gs_classifier_train(S, eta=0.1, maxloss=-math.inf, rounds=3):
+	'''
+	Train a linear classifier from a matrix of training data (S) where the last column are the classes
+	
+	Parameters
+	----------
+	S : array_like
+		The training data
+	eta : float
+		Learning rate (default: 0.1)
+	maxloss : float
+		Provides an upper bound for the loss to terminate early (default: -inf)
+	rounds : int
+		After visiting each possible batch, the training data is reshuffled
+		and iterated again. This parameter controls how many times to do this
+		(default: 3)
+	'''
 
-	def shuffle(A,b):
-		r = np.arange(0, N)
-		np.random.shuffle(r)
-		return A[r,:], b[r]
-
+	N,M = S.shape
 	# add a dummy column of all ones to training data for the vector bias
-	A = np.append(A, np.ones((A.shape[0],1)), axis=1)
-	N,M = A.shape
-
-	# if maxiters is not supplied, calculate maxiters from the number of rows
-	maxiters = maxiters if maxiters is not None else N*5
+	S = np.insert(S, -1, np.ones(N), axis=1)
+	A = S[:,:-1]
+	b = S[:,-1]
 
 	iterations = 0
 	best_solution = np.zeros(M, dtype='double')
 	best_loss = math.inf
 	while True:
-		# select rows for this iteration
+		# select rows for this iteration in a sliding window fashion
 		i = np.arange(iterations, iterations+M) % N
 		x0 = gauss_seidel(A[i,:], b[i], best_solution)
 		iterations += 1
 		if x0 is None:
 			# if this problem didn't converge, shuffle the training data
 			# hopefully we won't get any more subproblems that diverge
-			A, b = shuffle(A, b)
+			np.random.shuffle(S)
 			continue
+
+		# eta is the learning rate
+		x0 = best_solution * (1-eta) + eta * x0
 
 		# make predictions over the training data and calculate loss
 		predictions = (A @ x0) * b
-		loss = -predictions[predictions<0].sum()
+		loss = np.maximum(0, -predictions).sum()
 		# if this is a better solution, update the vector
 		if loss < best_loss:
 			best_solution, best_loss = x0, loss
-			# return if all samples were correctly classified
-			if (predictions > 0).all():
-				break
 			# return if the loss is acceptable
 			if loss < maxloss:
 				break
+			# reset iteration counter and shuffle
 			iterations = 0
-			# reorder the training data so that the best classified
-			# samples are at the beginning
-			s = np.argsort(predictions)
-			A, b = A[-s,:], b[-s]
-		if iterations >= maxiters:
-			break
+			np.random.shuffle(S)
+		if iterations >= N:
+			if rounds <= 0:
+				break
+			else:
+				np.random.shuffle(S)
+				iterations = 0
+				rounds -= 1
 	return best_solution, best_loss
 
 if __name__ == '__main__':
@@ -68,8 +79,5 @@ if __name__ == '__main__':
 		[3.2, 1.5, 1],
 		[4, 4, -1]
 	])
-	t0 = time.time()
-	xf = gs_classifier_train(dataset[:,0:2],dataset[:,2])
-	t1 = time.time()
+	xf = gs_classifier_train(dataset)
 	print(xf)
-	print(t1-t0)
